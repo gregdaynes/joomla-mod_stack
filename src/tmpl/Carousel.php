65e6b0671f5f7f382c9e90797c39fdaf539f18bd
Gregory Daynes <gregdaynes@gmail.com>
2013-12-26 11:53:08 -0800

    Picturefill now included

--- src/tmpl/Carousel.php
+++ src/tmpl/Carousel.php
@@ -3,12 +3,13 @@ defined( '_JEXEC' ) or die( 'Restricted access');
 
 $document = JFactory::getDocument();
 $document->addStyleSheet(JURI::base() . 'media/mod_stack/css/mod_stack.css');
+$document->addScript(JURI::base() . 'media/mod_stack/js/picturefill.js');
 $template   = $params->get('template', 'Carousel');
 $moduleclass_sfx = $params->get('moduleclass_sfx', $template);
 ?>
 
 
-<div class="stack  stack--<?php echo $moduleclass_sfx; ?>">
+<div class="stack  stack--carousel stack--<?php echo $moduleclass_sfx; ?>">
 
   <?php foreach($list as $index=>$item) : ?>
   <input type="radio" name="stack--<?php $moduleclass_sfx; ?>" id="stack--<?php echo $moduleclass_sfx; ?>__item<?php echo $index; ?>" class="stack__radio-control  is-hidden" <?php if ($index == 0) : ?>checked<?php endif; ?> />
@@ -22,7 +23,15 @@ $moduleclass_sfx = $params->get('moduleclass_sfx', $template);
       <article class="stack__item">
         <figure>
           <div class="stack__img-wrapper">
+            <span class="stack__img" data-picture data-alt="<!-- $img-alt | Alt Text -->">
+              <span data-src="http://placehold.it/320x180"></span>
+              <span data-src="http://placehold.it/569x320" data-media="(min-width: 480px)"></span>
+              <span data-src="http://placehold.it/853x480" data-media="(min-width: 768px)"></span>
 
+              <noscript>
+                  <img src="http://placehold.it/320x320" alt="<!-- $img-alt | Alt Text -->">
+              </noscript>
+            </span>
           </div>
 
           <figcaption class="stack__caption">
